1ce8275a90ce8bb99539436b91cbe52ccc2e0338
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-05-06 20:29:56 -0500

    pequena modificacion

--- src/main/java/pe/edu/cibertec/escuelafutbol/model/bd/Profesores.java
+++ src/main/java/pe/edu/cibertec/escuelafutbol/model/bd/Profesores.java
@@ -1,8 +1,6 @@
 package pe.edu.cibertec.escuelafutbol.model.bd;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -10,6 +8,7 @@ import lombok.Data;
 @Table(name = "profesores")
 public class Profesores {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String apellido;
